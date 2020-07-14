66de9df9062f4ca75dfc47464cc216c25c32a549
Daomtthuan <dao.mt.thuan@gmail.com>
2020-07-14 23:39:47 +0700

    update

--- PMSX/PMSX/View/Form/Insert/BiopsyGroup.Designer.cs
+++ PMSX/PMSX/View/Form/Insert/BiopsyGroup.Designer.cs
@@ -24,25 +24,21 @@
         /// </summary>
         private void InitializeComponent() {
       this.layoutControl = new DevExpress.XtraLayout.LayoutControl();
-      this.usernameInput = new DevExpress.XtraEditors.TextEdit();
-      this.nameInput = new DevExpress.XtraEditors.TextEdit();
+      this.codeInput = new DevExpress.XtraEditors.TextEdit();
       this.insertButton = new DevExpress.XtraEditors.SimpleButton();
       this.commentInput = new DevExpress.XtraEditors.MemoEdit();
       this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-      this.usernameInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
-      this.nameInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
+      this.codeInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
       this.insertButtonLayout = new DevExpress.XtraLayout.LayoutControlItem();
       this.requiredLabel = new DevExpress.XtraLayout.SimpleLabelItem();
       this.notRequiredLabel = new DevExpress.XtraLayout.SimpleLabelItem();
       this.commentInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
       ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).BeginInit();
       this.layoutControl.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInput.Properties)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInput.Properties)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInput.Properties)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInput.Properties)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInputLayout)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInputLayout)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInputLayout)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.insertButtonLayout)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.requiredLabel)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.notRequiredLabel)).BeginInit();
@@ -52,8 +48,7 @@
       // layoutControl
       // 
       this.layoutControl.AllowCustomization = false;
-      this.layoutControl.Controls.Add(this.usernameInput);
-      this.layoutControl.Controls.Add(this.nameInput);
+      this.layoutControl.Controls.Add(this.codeInput);
       this.layoutControl.Controls.Add(this.insertButton);
       this.layoutControl.Controls.Add(this.commentInput);
       this.layoutControl.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -65,23 +60,14 @@
       this.layoutControl.TabIndex = 1;
       this.layoutControl.Text = "layoutControl";
       // 
-      // usernameInput
+      // codeInput
       // 
-      this.usernameInput.Location = new System.Drawing.Point(12, 71);
-      this.usernameInput.Name = "usernameInput";
-      this.usernameInput.Properties.MaxLength = 100;
-      this.usernameInput.Size = new System.Drawing.Size(476, 28);
-      this.usernameInput.StyleController = this.layoutControl;
-      this.usernameInput.TabIndex = 4;
-      // 
-      // nameInput
-      // 
-      this.nameInput.Location = new System.Drawing.Point(12, 127);
-      this.nameInput.Name = "nameInput";
-      this.nameInput.Properties.MaxLength = 100;
-      this.nameInput.Size = new System.Drawing.Size(476, 28);
-      this.nameInput.StyleController = this.layoutControl;
-      this.nameInput.TabIndex = 5;
+      this.codeInput.Location = new System.Drawing.Point(12, 71);
+      this.codeInput.Name = "codeInput";
+      this.codeInput.Properties.MaxLength = 100;
+      this.codeInput.Size = new System.Drawing.Size(476, 28);
+      this.codeInput.StyleController = this.layoutControl;
+      this.codeInput.TabIndex = 4;
       // 
       // insertButton
       // 
@@ -98,10 +84,10 @@
       // 
       // commentInput
       // 
-      this.commentInput.Location = new System.Drawing.Point(12, 228);
+      this.commentInput.Location = new System.Drawing.Point(12, 172);
       this.commentInput.Name = "commentInput";
       this.commentInput.Properties.MaxLength = 500;
-      this.commentInput.Size = new System.Drawing.Size(476, 136);
+      this.commentInput.Size = new System.Drawing.Size(476, 192);
       this.commentInput.StyleController = this.layoutControl;
       this.commentInput.TabIndex = 7;
       // 
@@ -110,8 +96,7 @@
       this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
       this.Root.GroupBordersVisible = false;
       this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.usernameInputLayout,
-            this.nameInputLayout,
+            this.codeInputLayout,
             this.insertButtonLayout,
             this.requiredLabel,
             this.notRequiredLabel,
@@ -120,25 +105,15 @@
       this.Root.Size = new System.Drawing.Size(500, 426);
       this.Root.TextVisible = false;
       // 
-      // usernameInputLayout
+      // codeInputLayout
       // 
-      this.usernameInputLayout.Control = this.usernameInput;
-      this.usernameInputLayout.Location = new System.Drawing.Point(0, 35);
-      this.usernameInputLayout.Name = "usernameInputLayout";
-      this.usernameInputLayout.Size = new System.Drawing.Size(480, 56);
-      this.usernameInputLayout.Text = "Tên đăng nhập";
-      this.usernameInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
-      this.usernameInputLayout.TextSize = new System.Drawing.Size(190, 21);
-      // 
-      // nameInputLayout
-      // 
-      this.nameInputLayout.Control = this.nameInput;
-      this.nameInputLayout.Location = new System.Drawing.Point(0, 91);
-      this.nameInputLayout.Name = "nameInputLayout";
-      this.nameInputLayout.Size = new System.Drawing.Size(480, 56);
-      this.nameInputLayout.Text = "Tên";
-      this.nameInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
-      this.nameInputLayout.TextSize = new System.Drawing.Size(190, 21);
+      this.codeInputLayout.Control = this.codeInput;
+      this.codeInputLayout.Location = new System.Drawing.Point(0, 35);
+      this.codeInputLayout.Name = "codeInputLayout";
+      this.codeInputLayout.Size = new System.Drawing.Size(480, 56);
+      this.codeInputLayout.Text = "Mã số";
+      this.codeInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
+      this.codeInputLayout.TextSize = new System.Drawing.Size(190, 21);
       // 
       // insertButtonLayout
       // 
@@ -167,7 +142,7 @@
       this.notRequiredLabel.AllowHotTrack = false;
       this.notRequiredLabel.AppearanceItemCaption.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Bold);
       this.notRequiredLabel.AppearanceItemCaption.Options.UseFont = true;
-      this.notRequiredLabel.Location = new System.Drawing.Point(0, 147);
+      this.notRequiredLabel.Location = new System.Drawing.Point(0, 91);
       this.notRequiredLabel.Name = "notRequiredLabel";
       this.notRequiredLabel.Size = new System.Drawing.Size(480, 45);
       this.notRequiredLabel.Spacing = new DevExpress.XtraLayout.Utils.Padding(0, 0, 10, 10);
@@ -177,14 +152,14 @@
       // commentInputLayout
       // 
       this.commentInputLayout.Control = this.commentInput;
-      this.commentInputLayout.Location = new System.Drawing.Point(0, 192);
+      this.commentInputLayout.Location = new System.Drawing.Point(0, 136);
       this.commentInputLayout.Name = "commentInputLayout";
-      this.commentInputLayout.Size = new System.Drawing.Size(480, 164);
+      this.commentInputLayout.Size = new System.Drawing.Size(480, 220);
       this.commentInputLayout.Text = "Ghi chú";
       this.commentInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
       this.commentInputLayout.TextSize = new System.Drawing.Size(190, 21);
       // 
-      // Staff
+      // BiopsyGroup
       // 
       this.AcceptButton = this.insertButton;
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
@@ -193,17 +168,15 @@
       this.ClientSize = new System.Drawing.Size(500, 426);
       this.Controls.Add(this.layoutControl);
       this.MaximizeBox = false;
-      this.Name = "Staff";
+      this.Name = "BiopsyGroup";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-      this.Text = "PMSX - Thêm nhân viên";
+      this.Text = "PMSX - Thêm nhóm sinh thiết";
       ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).EndInit();
       this.layoutControl.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInput.Properties)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInput.Properties)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInput.Properties)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInput.Properties)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInputLayout)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInputLayout)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInputLayout)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.insertButtonLayout)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.requiredLabel)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.notRequiredLabel)).EndInit();
@@ -215,12 +188,10 @@
         #endregion
 
         private DevExpress.XtraLayout.LayoutControl layoutControl;
-        private DevExpress.XtraEditors.TextEdit usernameInput;
-        private DevExpress.XtraEditors.TextEdit nameInput;
+        private DevExpress.XtraEditors.TextEdit codeInput;
         private DevExpress.XtraEditors.SimpleButton insertButton;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private DevExpress.XtraLayout.LayoutControlItem usernameInputLayout;
-        private DevExpress.XtraLayout.LayoutControlItem nameInputLayout;
+        private DevExpress.XtraLayout.LayoutControlItem codeInputLayout;
         private DevExpress.XtraLayout.LayoutControlItem insertButtonLayout;
         private DevExpress.XtraEditors.MemoEdit commentInput;
         private DevExpress.XtraLayout.SimpleLabelItem requiredLabel;
--- PMSX/PMSX/View/Form/Update/BiopsyGroup.Designer.cs
+++ PMSX/PMSX/View/Form/Update/BiopsyGroup.Designer.cs
@@ -24,38 +24,38 @@
         /// </summary>
         private void InitializeComponent() {
       this.layoutControl = new DevExpress.XtraLayout.LayoutControl();
-      this.usernameInput = new DevExpress.XtraEditors.TextEdit();
-      this.nameInput = new DevExpress.XtraEditors.TextEdit();
       this.insertButton = new DevExpress.XtraEditors.SimpleButton();
       this.commentInput = new DevExpress.XtraEditors.MemoEdit();
       this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-      this.usernameInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
-      this.nameInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
       this.insertButtonLayout = new DevExpress.XtraLayout.LayoutControlItem();
       this.requiredLabel = new DevExpress.XtraLayout.SimpleLabelItem();
       this.notRequiredLabel = new DevExpress.XtraLayout.SimpleLabelItem();
       this.commentInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
+      this.codeInput = new DevExpress.XtraEditors.TextEdit();
+      this.codeInputLayout = new DevExpress.XtraLayout.LayoutControlItem();
+      this.stateRadio = new DevExpress.XtraEditors.RadioGroup();
+      this.stateRadioLayout = new DevExpress.XtraLayout.LayoutControlItem();
       ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).BeginInit();
       this.layoutControl.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInput.Properties)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInput.Properties)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInput.Properties)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInputLayout)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInputLayout)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.insertButtonLayout)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.requiredLabel)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.notRequiredLabel)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInputLayout)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInput.Properties)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInputLayout)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.stateRadio.Properties)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.stateRadioLayout)).BeginInit();
       this.SuspendLayout();
       // 
       // layoutControl
       // 
       this.layoutControl.AllowCustomization = false;
-      this.layoutControl.Controls.Add(this.usernameInput);
-      this.layoutControl.Controls.Add(this.nameInput);
       this.layoutControl.Controls.Add(this.insertButton);
       this.layoutControl.Controls.Add(this.commentInput);
+      this.layoutControl.Controls.Add(this.codeInput);
+      this.layoutControl.Controls.Add(this.stateRadio);
       this.layoutControl.Dock = System.Windows.Forms.DockStyle.Fill;
       this.layoutControl.Location = new System.Drawing.Point(0, 0);
       this.layoutControl.Name = "layoutControl";
@@ -64,24 +64,6 @@
       this.layoutControl.Size = new System.Drawing.Size(500, 426);
       this.layoutControl.TabIndex = 1;
       this.layoutControl.Text = "layoutControl";
-      // 
-      // usernameInput
-      // 
-      this.usernameInput.Location = new System.Drawing.Point(12, 71);
-      this.usernameInput.Name = "usernameInput";
-      this.usernameInput.Properties.MaxLength = 100;
-      this.usernameInput.Size = new System.Drawing.Size(476, 28);
-      this.usernameInput.StyleController = this.layoutControl;
-      this.usernameInput.TabIndex = 4;
-      // 
-      // nameInput
-      // 
-      this.nameInput.Location = new System.Drawing.Point(12, 127);
-      this.nameInput.Name = "nameInput";
-      this.nameInput.Properties.MaxLength = 100;
-      this.nameInput.Size = new System.Drawing.Size(476, 28);
-      this.nameInput.StyleController = this.layoutControl;
-      this.nameInput.TabIndex = 5;
       // 
       // insertButton
       // 
@@ -98,10 +80,10 @@
       // 
       // commentInput
       // 
-      this.commentInput.Location = new System.Drawing.Point(12, 228);
+      this.commentInput.Location = new System.Drawing.Point(12, 229);
       this.commentInput.Name = "commentInput";
       this.commentInput.Properties.MaxLength = 500;
-      this.commentInput.Size = new System.Drawing.Size(476, 136);
+      this.commentInput.Size = new System.Drawing.Size(476, 135);
       this.commentInput.StyleController = this.layoutControl;
       this.commentInput.TabIndex = 7;
       // 
@@ -110,35 +92,15 @@
       this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
       this.Root.GroupBordersVisible = false;
       this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.usernameInputLayout,
-            this.nameInputLayout,
             this.insertButtonLayout,
             this.requiredLabel,
             this.notRequiredLabel,
-            this.commentInputLayout});
+            this.commentInputLayout,
+            this.codeInputLayout,
+            this.stateRadioLayout});
       this.Root.Name = "Root";
       this.Root.Size = new System.Drawing.Size(500, 426);
       this.Root.TextVisible = false;
-      // 
-      // usernameInputLayout
-      // 
-      this.usernameInputLayout.Control = this.usernameInput;
-      this.usernameInputLayout.Location = new System.Drawing.Point(0, 35);
-      this.usernameInputLayout.Name = "usernameInputLayout";
-      this.usernameInputLayout.Size = new System.Drawing.Size(480, 56);
-      this.usernameInputLayout.Text = "Tên đăng nhập";
-      this.usernameInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
-      this.usernameInputLayout.TextSize = new System.Drawing.Size(190, 21);
-      // 
-      // nameInputLayout
-      // 
-      this.nameInputLayout.Control = this.nameInput;
-      this.nameInputLayout.Location = new System.Drawing.Point(0, 91);
-      this.nameInputLayout.Name = "nameInputLayout";
-      this.nameInputLayout.Size = new System.Drawing.Size(480, 56);
-      this.nameInputLayout.Text = "Tên";
-      this.nameInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
-      this.nameInputLayout.TextSize = new System.Drawing.Size(190, 21);
       // 
       // insertButtonLayout
       // 
@@ -167,7 +129,7 @@
       this.notRequiredLabel.AllowHotTrack = false;
       this.notRequiredLabel.AppearanceItemCaption.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Bold);
       this.notRequiredLabel.AppearanceItemCaption.Options.UseFont = true;
-      this.notRequiredLabel.Location = new System.Drawing.Point(0, 147);
+      this.notRequiredLabel.Location = new System.Drawing.Point(0, 148);
       this.notRequiredLabel.Name = "notRequiredLabel";
       this.notRequiredLabel.Size = new System.Drawing.Size(480, 45);
       this.notRequiredLabel.Spacing = new DevExpress.XtraLayout.Utils.Padding(0, 0, 10, 10);
@@ -177,14 +139,62 @@
       // commentInputLayout
       // 
       this.commentInputLayout.Control = this.commentInput;
-      this.commentInputLayout.Location = new System.Drawing.Point(0, 192);
+      this.commentInputLayout.Location = new System.Drawing.Point(0, 193);
       this.commentInputLayout.Name = "commentInputLayout";
-      this.commentInputLayout.Size = new System.Drawing.Size(480, 164);
+      this.commentInputLayout.Size = new System.Drawing.Size(480, 163);
       this.commentInputLayout.Text = "Ghi chú";
       this.commentInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
       this.commentInputLayout.TextSize = new System.Drawing.Size(190, 21);
       // 
-      // Staff
+      // codeInput
+      // 
+      this.codeInput.Location = new System.Drawing.Point(12, 71);
+      this.codeInput.Name = "codeInput";
+      this.codeInput.Properties.MaxLength = 100;
+      this.codeInput.Size = new System.Drawing.Size(476, 28);
+      this.codeInput.StyleController = this.layoutControl;
+      this.codeInput.TabIndex = 4;
+      // 
+      // codeInputLayout
+      // 
+      this.codeInputLayout.Control = this.codeInput;
+      this.codeInputLayout.CustomizationFormText = "Mã số";
+      this.codeInputLayout.Location = new System.Drawing.Point(0, 35);
+      this.codeInputLayout.Name = "codeInputLayout";
+      this.codeInputLayout.Size = new System.Drawing.Size(480, 56);
+      this.codeInputLayout.Text = "Mã số";
+      this.codeInputLayout.TextLocation = DevExpress.Utils.Locations.Top;
+      this.codeInputLayout.TextSize = new System.Drawing.Size(190, 21);
+      // 
+      // stateRadio
+      // 
+      this.stateRadio.Location = new System.Drawing.Point(12, 127);
+      this.stateRadio.Name = "stateRadio";
+      this.stateRadio.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+      this.stateRadio.Properties.Appearance.Options.UseBackColor = true;
+      this.stateRadio.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+      this.stateRadio.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(1, "Kích hoạt"),
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(0, "Vô hiệu hoá")});
+      this.stateRadio.Size = new System.Drawing.Size(476, 29);
+      this.stateRadio.StyleController = this.layoutControl;
+      this.stateRadio.TabIndex = 8;
+      // 
+      // stateRadioLayout
+      // 
+      this.stateRadioLayout.Control = this.stateRadio;
+      this.stateRadioLayout.CustomizationFormText = "Trạng thái";
+      this.stateRadioLayout.Location = new System.Drawing.Point(0, 91);
+      this.stateRadioLayout.MaxSize = new System.Drawing.Size(480, 57);
+      this.stateRadioLayout.MinSize = new System.Drawing.Size(480, 57);
+      this.stateRadioLayout.Name = "stateRadioLayout";
+      this.stateRadioLayout.Size = new System.Drawing.Size(480, 57);
+      this.stateRadioLayout.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+      this.stateRadioLayout.Text = "Trạng thái";
+      this.stateRadioLayout.TextLocation = DevExpress.Utils.Locations.Top;
+      this.stateRadioLayout.TextSize = new System.Drawing.Size(190, 21);
+      // 
+      // BiopsyGroup
       // 
       this.AcceptButton = this.insertButton;
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
@@ -193,21 +203,21 @@
       this.ClientSize = new System.Drawing.Size(500, 426);
       this.Controls.Add(this.layoutControl);
       this.MaximizeBox = false;
-      this.Name = "Staff";
+      this.Name = "BiopsyGroup";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "PMSX - Thêm nhân viên";
       ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).EndInit();
       this.layoutControl.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInput.Properties)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInput.Properties)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInput.Properties)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.usernameInputLayout)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.nameInputLayout)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.insertButtonLayout)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.requiredLabel)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.notRequiredLabel)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.commentInputLayout)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInput.Properties)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.codeInputLayout)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.stateRadio.Properties)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.stateRadioLayout)).EndInit();
       this.ResumeLayout(false);
 
         }
@@ -215,16 +225,16 @@
         #endregion
 
         private DevExpress.XtraLayout.LayoutControl layoutControl;
-        private DevExpress.XtraEditors.TextEdit usernameInput;
-        private DevExpress.XtraEditors.TextEdit nameInput;
         private DevExpress.XtraEditors.SimpleButton insertButton;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private DevExpress.XtraLayout.LayoutControlItem usernameInputLayout;
-        private DevExpress.XtraLayout.LayoutControlItem nameInputLayout;
         private DevExpress.XtraLayout.LayoutControlItem insertButtonLayout;
         private DevExpress.XtraEditors.MemoEdit commentInput;
         private DevExpress.XtraLayout.SimpleLabelItem requiredLabel;
         private DevExpress.XtraLayout.SimpleLabelItem notRequiredLabel;
         private DevExpress.XtraLayout.LayoutControlItem commentInputLayout;
-    }
+    private DevExpress.XtraEditors.TextEdit codeInput;
+    private DevExpress.XtraLayout.LayoutControlItem codeInputLayout;
+    private DevExpress.XtraEditors.RadioGroup stateRadio;
+    private DevExpress.XtraLayout.LayoutControlItem stateRadioLayout;
+  }
 }
